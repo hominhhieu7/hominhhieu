06b4a1d043bada794759e2460e2409d961aca12c
Ho Minh Hieu <hominhhieu2502@gmail.com>
2020-03-11 20:43:05 +0700

    fix

--- components/Top.js
+++ components/Top.js
@@ -3,9 +3,9 @@ import { Menu, Row, Col } from 'antd';
 import {
     InfoCircleTwoTone,
     GithubOutlined,
-    SettingOutlined,
+    NotificationFilled,
 } from '@ant-design/icons';
-
+import Link from 'next/link'
 
 const { SubMenu } = Menu;
 
@@ -25,46 +25,39 @@ export default class Top extends Component {
             <div>
                 <Row>
                     <Col span={1}>
-                        <a href="#">
-                            <h2>
-                                Hieu
-              </h2>
-                        </a>
+                        <Link  href="/">
+                            <a>
+                                <h2>
+                                    Hieu
+                                </h2>
+                            </a>
+                        </Link>
                     </Col>
                     <Col span={16} offset={5}>
                         <Menu style={{ lineHeight: "63px" }} onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
                             <Menu.Item key="infor">
-                                <a href="#">
-                                    <InfoCircleTwoTone style={{paddingRight: "10px"}} />
+                                <Link href="/about">
+                                    <a>
+                                        <InfoCircleTwoTone style={{ paddingRight: "10px" }} />
                                 About Me
                                 </a>
+                                </Link>
                             </Menu.Item>
                             <Menu.Item key="github">
                                 <a href="https://github.com/hominhhieu7" target="_blank">
-                                    <GithubOutlined  style={{paddingRight: "10px"}} />
+                                    <GithubOutlined style={{ paddingRight: "10px" }} />
                                  My Github
                                  </a>
                             </Menu.Item>
                             <SubMenu
                                 title={
                                     <span className="submenu-title-wrapper">
-                                        <SettingOutlined />
-              Navigation Three - SubMenu
-              </span>}>
-                                <Menu.ItemGroup title="Item 1">
-                                    <Menu.Item key="setting:1">Option 1</Menu.Item>
-                                    <Menu.Item key="setting:2">Option 2</Menu.Item>
-                                </Menu.ItemGroup>
-                                <Menu.ItemGroup title="Item 2">
-                                    <Menu.Item key="setting:3">Option 3</Menu.Item>
-                                    <Menu.Item key="setting:4">Option 4</Menu.Item>
-                                </Menu.ItemGroup>
+                                        <NotificationFilled />
+                                            My Blog
+                                    </span>}>
+                                <Menu.Item key="setting:1">Coming soon</Menu.Item>
+                                <Menu.Item key="setting:2">Coming soon</Menu.Item>
                             </SubMenu>
-                            <Menu.Item key="alipay">
-                                <a href="" rel="noopener noreferrer">
-                                    Navigation Four - Link
-          </a>
-                            </Menu.Item>
                         </Menu>
                     </Col>
                 </Row>
