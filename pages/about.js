c1bf41fc0b16246dfd617ccae69243957998dbb6
Ho Minh Hieu <hominhhieu2502@gmail.com>
2020-04-29 17:05:55 +0700

    fix

--- pages/about.js
+++ pages/about.js
@@ -33,7 +33,7 @@ export default class About extends Component {
                     <Timeline pending="Finding job..." reverse={false} mode="left" style={{ width: "310px", lineHeight: '2' }}>
                         <Timeline.Item label="08/2015" >Study in Thu Dau Mot University</Timeline.Item>
                         <Timeline.Item label="06/2018 - 08/2018">Internship in Netco Post.</Timeline.Item>
-                        <Timeline.Item label="09/2018 - 12/2018">Web deisgn in Smartlog(SEM).</Timeline.Item>
+                        <Timeline.Item label="08/2018 - 12/2018">Web deisgn in Smartlog(SEM).</Timeline.Item>
                         <Timeline.Item label="04/2019">Learning online React.js in Coursera.org</Timeline.Item>
                         <Timeline.Item label="05/2019">Created app using React + Redux.</Timeline.Item>
                         <Timeline.Item label="06/2019">Learning Nodejs, Express.js, MongoDB</Timeline.Item>
